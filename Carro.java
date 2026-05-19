bbac3dc8d108eabeb0707071430d8f1a2d8d26be
IghorAires <1352314143@ulife.com.br>
2026-05-19 19:35:51 -0300

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,19 @@
+public class Carro {
+    String marca;
+    String modelo;
+    int ano;
+    public Carro(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    public void exibirDetalhes() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+    }
+    public static void main(String[] args) {
+        Carro carro = new Carro("Toyota", "Corolla", 2024);
+        carro.exibirDetalhes();
+    }
+}
